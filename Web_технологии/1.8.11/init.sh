ce48242103e00ca0fb8869b5a877e21bd914add0
Семенов Дмитрий <zend0@ya.ru>
2016-03-09 23:19:44 +0300

    Что бы симлинки без вопросов заменялись

--- Web_технологии/1.8.11/init.sh
+++ Web_технологии/1.8.11/init.sh
@@ -6,9 +6,9 @@ cp -f ~/stepic.org/Web_технологии/1.9.11/etc/* ~/web/etc
 cp -f ~/stepic.org/Web_технологии/1.9.11/web/* ~/web
 
 sudo rm /etc/nginx/sites-enabled/default
-sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
 
 # NEXT
