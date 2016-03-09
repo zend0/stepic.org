88434faf4e2af0087c58517b074ae632af2529ae
Семенов Дмитрий <zend0@ya.ru>
2016-03-09 22:46:03 +0300

    update

--- Web_технологии/1.6.7.py
+++ Web_технологии/1.6.7.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# http://parijatmishra.blogspot.ru/2008/01/writing-server-with-pythons-asyncore.html
+
 import asyncore
 import socket
 
--- Web_технологии/1.8.11/init.sh
+++ Web_технологии/1.8.11/init.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+mkdir -p ~/web/etc
+
+cp ~/stepic.org/Web_технологии/1.9.11/etc/* ~/web/etc
+cp ~/stepic.org/Web_технологии/1.9.11/web/* ~/web
+
+sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
 
 # NEXT
