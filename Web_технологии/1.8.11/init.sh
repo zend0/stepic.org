ffb11ee749921d7aeef195c6775f065fdbaf32dc
dsemenov <dsemenov@uptolike.ru>
2016-03-09 20:14:11 +0300

    Update scripts for step 1.9.11

--- Web_технологии/1.8.11/init.sh
+++ Web_технологии/1.8.11/init.sh
@@ -2,8 +2,8 @@
 
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-# sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
-# sudo /etc/init.d/gunicorn restart
+sudo ln -s /home/box/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart
 
 # NEXT
 # git clone https://github.com/your_account/stepic_web_project.git /home/box/web
