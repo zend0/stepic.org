455fcfdf76e4a210104eb0e9050fe73ee4ccfdde
Семенов Дмитрий <zend0@ya.ru>
2016-03-12 21:42:07 +0300

    update

--- web_technologies/2.1.11/ask/ask/urls.py
+++ web_technologies/2.1.11/ask/ask/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     url(r'^$', 'qa.views.test'),
     url(r'^login/.*$', 'qa.views.test'),
     url(r'^signup/.*$', 'qa.views.test'),
-    url(r'^questions/\d+/$', 'qa.views.test'),
+    url(r'^question/\d+/$', 'qa.views.test'),
     url(r'^ask/.*$', 'qa.views.test'),
     url(r'^popular/.*$', 'qa.views.test'),
     url(r'^new/.*$', 'qa.views.test')
