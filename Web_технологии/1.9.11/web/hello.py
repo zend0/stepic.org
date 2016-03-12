13cb6477c9ecd4204c948b9d9c8d14ba4e76469f
Семенов Дмитрий <zend0@ya.ru>
2016-03-12 19:40:27 +0300

    rename directory

--- Web_технологии/1.9.11/web/hello.py
+++
@@ -1,9 +0,0 @@
-
-def app(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-
-    resp = environ['QUERY_STRING'].split("&")
-    resp = [item+"\n" for item in resp]
-
-    return resp
-
