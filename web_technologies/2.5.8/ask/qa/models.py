634e6ae1ad7714188c70ddc8dfc7d81020aeebf4
dsemenov <dsemenov@uptolike.ru>
2016-03-23 19:28:26 +0300

    fix

--- web_technologies/2.5.8/ask/qa/models.py
+++ web_technologies/2.5.8/ask/qa/models.py
@@ -8,9 +8,9 @@ class Question(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
     added_at = models.DateTimeField(auto_now_add=True)
-    rating = models.ManyToManyField(User, related_name='likes_set')
+    rating = models.IntegerField(default=0)
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
-    # likes = models.ManyToManyField(User, related_name='likes_set')
+    likes = models.ManyToManyField(User, related_name='likes_set')
 
 
 class Answer(models.Model):
