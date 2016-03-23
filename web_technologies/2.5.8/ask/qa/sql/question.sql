515b2b595c65a6bf8a163e0dd3e7b3d6a38fdc2c
dsemenov <dsemenov@uptolike.ru>
2016-03-23 19:29:54 +0300

    fix

---
+++ web_technologies/2.5.8/ask/qa/sql/question.sql
@@ -0,0 +1,38 @@
+INSERT INTO qa_question (title, text, added_at, rating, author_id) VALUES ('Сколько будет 2+2?', 'Кто знает точный ответ, 2+2', '2016-03-21 18:32:34', 0, 1);
+INSERT INTO qa_question (title, text, added_at, rating, author_id) VALUES ('Сколько будет 3+3?', 'Кто знает точный ответ, 3+3', '2016-03-22 18:32:34', 2, 1);
+INSERT INTO qa_question (title, text, added_at, rating, author_id) VALUES ('Сколько будет 4+4?', 'Кто знает точный ответ, 4+4', '2016-03-23 18:45:34', 3, 1);
+INSERT INTO qa_question (title, text, added_at, rating, author_id) VALUES ('Сколько будет 1+1?', 'Кто знает точный ответ, 1+1', '2016-03-20 18:32:34', 0, 1);
+INSERT INTO qa_question (title, text, added_at, rating, author_id) VALUES ('Сколько будет 5+5?', 'Кто знает точный ответ, 5+5', '2016-03-18 18:36:34', 3, 1);
+INSERT INTO qa_question (title, text, added_at, rating, author_id) VALUES ('Сколько будет 6+6?', 'Кто знает точный ответ, 6+6', '2016-03-19 18:32:34', 7, 1);
+INSERT INTO qa_question (title, text, added_at, rating, author_id) VALUES ('Сколько будет 7+7?', 'Кто знает точный ответ, 7+7', '2016-03-17 18:32:34', 5, 1);
+INSERT INTO qa_question (title, text, added_at, rating, author_id) VALUES ('Сколько будет 8+8?', 'Кто знает точный ответ, 8+8', '2016-03-16 11:25:34', 6, 1);
+INSERT INTO qa_question (title, text, added_at, rating, author_id) VALUES ('Сколько будет 9+9?', 'Кто знает точный ответ, 9+9', '2016-03-16 18:32:34', 1, 1);
+INSERT INTO qa_question (title, text, added_at, rating, author_id) VALUES ('Сколько будет 1х1?', 'Кто знает точный ответ, 1х1', '2016-03-22 12:47:34', 2, 1);
+INSERT INTO qa_question (title, text, added_at, rating, author_id) VALUES ('Сколько будет 2х2?', 'Кто знает точный ответ, 2х2', '2016-03-22 10:32:34', 7, 1);
+INSERT INTO qa_question (title, text, added_at, rating, author_id) VALUES ('Сколько будет 3х3?', 'Кто знает точный ответ, 3х3', '2016-03-22 05:32:34', 10, 1);
+INSERT INTO qa_question (title, text, added_at, rating, author_id) VALUES ('Сколько будет 4х4?', 'Кто знает точный ответ, 4х4', '2016-03-17 10:12:34', 6, 1);
+INSERT INTO qa_question (title, text, added_at, rating, author_id) VALUES ('Сколько будет 5х5?', 'Кто знает точный ответ, 5х5', '2016-03-17 23:08:08', 3, 1);
+INSERT INTO qa_question (title, text, added_at, rating, author_id) VALUES ('Сколько будет 6х6?', 'Кто знает точный ответ, 6х6', '2016-03-17 08:30:34', 2, 1);
+INSERT INTO qa_question (title, text, added_at, rating, author_id) VALUES ('Сколько будет 7х7?', 'Кто знает точный ответ, 7х7', '2016-03-16 09:11:33', 0, 1);
+INSERT INTO qa_question (title, text, added_at, rating, author_id) VALUES ('Сколько будет 8х8?', 'Кто знает точный ответ, 8х8', '2016-03-16 09:12:11', 0, 1);
+INSERT INTO qa_question (title, text, added_at, rating, author_id) VALUES ('Сколько будет 9х9?', 'Кто знает точный ответ, 9х9', '2016-03-16 09:13:34', 4, 1);
+INSERT INTO qa_question (title, text, added_at, rating, author_id) VALUES ('Сколько будет 1-1?', 'Кто знает точный ответ, 1-1', '2016-03-15 21:44:00', 9, 1);
+INSERT INTO qa_question (title, text, added_at, rating, author_id) VALUES ('Сколько будет 2-2?', 'Кто знает точный ответ, 2-2', '2016-03-15 09:40:09', 2, 1);
+INSERT INTO qa_question (title, text, added_at, rating, author_id) VALUES ('Сколько будет 3-3?', 'Кто знает точный ответ, 3-3', '2016-03-14 12:07:59', 3, 1);
+INSERT INTO qa_question (title, text, added_at, rating, author_id) VALUES ('Сколько будет 4-4?', 'Кто знает точный ответ, 4-4', '2016-03-14 14:14:14', 1, 1);
+INSERT INTO qa_question (title, text, added_at, rating, author_id) VALUES ('Сколько будет 5-5?', 'Кто знает точный ответ, 5-5', '2016-03-13 13:32:13', 1, 1);
+INSERT INTO qa_question (title, text, added_at, rating, author_id) VALUES ('Сколько будет 6-6?', 'Кто знает точный ответ, 6-6', '2016-03-12 20:32:13', 2, 1);
+INSERT INTO qa_question (title, text, added_at, rating, author_id) VALUES ('Сколько будет 7-7?', 'Кто знает точный ответ, 7-7', '2016-03-12 19:22:13', 4, 1);
+INSERT INTO qa_question (title, text, added_at, rating, author_id) VALUES ('Сколько будет 8-8?', 'Кто знает точный ответ, 8-8', '2016-03-12 14:39:47', 6, 1);
+INSERT INTO qa_question (title, text, added_at, rating, author_id) VALUES ('Сколько будет 9-9?', 'Кто знает точный ответ, 9-9', '2016-03-12 12:14:29', 7, 1);
+INSERT INTO qa_question (title, text, added_at, rating, author_id) VALUES ('Сколько будет 3+1?', 'Кто знает точный ответ, 3+1', '2016-03-11 11:11:49', 8, 1);
+INSERT INTO qa_question (title, text, added_at, rating, author_id) VALUES ('Сколько будет 3+1?', 'Кто знает точный ответ, 3+1', '2016-03-11 12:10:49', 4, 1);
+INSERT INTO qa_question (title, text, added_at, rating, author_id) VALUES ('Сколько будет 3+2?', 'Кто знает точный ответ, 3+2', '2016-03-11 10:21:49', 2, 1);
+INSERT INTO qa_question (title, text, added_at, rating, author_id) VALUES ('Сколько будет 4+1?', 'Кто знает точный ответ, 4+1', '2016-03-11 09:00:49', 1, 1);
+INSERT INTO qa_question (title, text, added_at, rating, author_id) VALUES ('Сколько будет 2+4?', 'Кто знает точный ответ, 2+4', '2016-03-11 08:49:49', 3, 1);
+INSERT INTO qa_question (title, text, added_at, rating, author_id) VALUES ('Сколько будет 2+3?', 'Кто знает точный ответ, 2+3', '2016-03-11 08:33:49', 0, 1);
+INSERT INTO qa_question (title, text, added_at, rating, author_id) VALUES ('Сколько будет 2+1?', 'Кто знает точный ответ, 2+1', '2016-03-11 08:17:49', 1, 1);
+INSERT INTO qa_question (title, text, added_at, rating, author_id) VALUES ('Сколько будет 9+1?', 'Кто знает точный ответ, 9+1', '2016-03-11 07:01:49', 0, 1);
+
+
+
