f9ded954fe40caa5b4dd81cb26d85bf515664751
dsemenov <dsemenov@uptolike.ru>
2016-04-20 19:15:17 +0300

    upd

--- python_osnovi_i_primenenie/test.py
+++ python_osnovi_i_primenenie/test.py
@@ -12,3 +12,34 @@ print(sys.getrefcount('Hello'))
 a = 'Bye'
 print(sys.getrefcount('Hello'))
 print(b)
+
+print('-----------')
+
+def h():
+    print("- H")
+    print("- 12")
+    print("H - end")
+
+
+def f():
+    print("- F")
+    g(h)
+    print("F - end")
+
+
+def g(a):
+    print("- G - ", a)
+    a()
+    print("G - end")
+
+print("\nMODULE")
+g(f)
+
+print('-----------')
+
+
+def testt(*fi):
+    print(fi)
+
+lst = ['aa', 'bb']
+testt('a', 'b', 'c', 'd')
