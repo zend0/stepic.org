316c6b588e3920795939196fb7a616cddc08b04a
dsemenov <dsemenov@uptolike.ru>
2016-04-27 20:15:38 +0300

    upd

--- python_osnovi_i_primenenie/1.6.7.py
+++ python_osnovi_i_primenenie/1.6.7.py
@@ -6,25 +6,38 @@ def mysplit(mystr, spl=''):
     if spl == ':':
         k = mystr.split(spl)
         if len(k) > 1:
-            return {k[0].strip(): mysplit(k[1])}
+            if k[0].strip() in classes:
+                classes[k[0].strip()].extend(mysplit(k[1]))
+            else:
+                classes.update({k[0].strip(): mysplit(k[1])})
         else:
-            return {k[0].strip(): []}
+            classes.update({k[0].strip(): []})
     else:
         return mystr.split()
 
-
 def mycheck(mystr):
-    k = mystr.split()
+    a, b = mystr.split()
     # print(k)
     # print(classes)
-    if k[1] in classes:
+    if b in classes:
         # print(k[0])
         # print(classes[k[1]])
-        if k[0] in classes[k[1]]:
+        if a in classes[b] or a == b:
             return 'Yes'
         else:
             return 'No'
     else:
+        return 'No'
+
+def newcheck(a, b):
+    if b in classes:
+        print('** ' + b + ' in classes')
+        for i in classes[b]:
+            print(' ** ' + i + ' in ' + str(classes[b]))
+            if a == i:
+                return 'Yes'
+            else:
+                return newcheck(i, a)
         return 'No'
 
 classes = dict()
@@ -34,16 +47,19 @@ n = int(input())
 for ni in range(n):
     dd = str(input())
 """
-n = ['A', 'B : A', 'C : A', 'D : B C']
+n = ['A', 'B : A', 'C : A', 'D : B C', 'C : E']
 for ni in n:
-    dd = ni
-    classes.update(mysplit(dd, ':'))
+    mysplit(ni, ':')
 
+print(classes)
 """
 q = int(input())
 for qi in range(q):
     print(mycheck(str(input())))
 """
-q = ['A B', 'B D', 'C D', 'D A']
+q = ['A B', 'B D', 'C D', 'D A', 'A A', 'A D']
 for qi in q:
-    print(mycheck(qi))
+    #print(mycheck(qi))
+    a, b = qi.split()
+    print(newcheck(a, b))
+
