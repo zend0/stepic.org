bb648e0423af3937595759e055a3c7809483c1e6
Семенов Дмитрий <zend0@ya.ru>
2016-05-01 23:12:17 +0300

    upd не работает

--- python_osnovi_i_primenenie/2.1.7.py
+++ python_osnovi_i_primenenie/2.1.7.py
@@ -33,7 +33,7 @@ class ConvData:
     def sparent(self, val):
         del self.parlst[:]
         self.gparent(val)
-        print('>>> ' + val + ' - ' + str(self.parlst))
+        # print('>>> ' + val + ' - ' + str(self.parlst))
         for i in range(len(self.parlst)):
             if val == self.parlst[i]:
                 self.parlst.pop(i)
@@ -43,23 +43,25 @@ x = ConvData()
 
 # n = range(int(input()))
 # n = ['ArithmeticError', 'ZeroDivisionError : ArithmeticError', 'OSError', 'FileNotFoundError : OSError']
-n = ["10", "11 : 10", "12 : 11", "20", "13 : 11 20", "21 : 20"]
+# n = ["10", "11 : 10", "12 : 11", "20", "13 : 11 20", "21 : 20"]
+n = ['1 : 2 3 4', '2 : 5 3 6 7 8 9', '5 : 8', '3 : 4 9', '10 : 7', '4', '6 : 3 4', '7 : 6', '8 : 10 4 6 7', '9']
 for i in n:
     x.dadd(i)
     # x.dadd(str(input()))
+
 """
 m = range(int(input()))
 for i in m:
     ml.append(str(input()))
 """
 # ml = ['ZeroDivisionError', 'OSError', 'ArithmeticError', 'FileNotFoundError']
-ml = ["20", "13", "10", "11", "21", "12"]
+# ml = ["20", "13", "10", "11", "21", "12"]
+ml = ['6', '9', '4', '5', '10', '1', '3', '2', '8', '7']
 for i in range(len(ml)):
-    print('in ' + str(ml[i]))
+    # print('in ' + str(ml[i]))
     x.sparent(ml[i])
     for pi in x.parlst:
         if pi in ml[0:i+1]:
-            # print(ml[i])
-            print('out ' + str(ml[i]))
+            print(ml[i])
 
 print(x.cls)
