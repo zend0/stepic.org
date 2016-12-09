a8a166c05468ce642b34f39b0e1ffc9673f0fbe5
dsemenov <dsemenov@uptolike.ru>
2016-12-09 17:11:03 +0300

    add new tasks

---
+++ algorithms.theory_and_practice/2.2.6.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def fib(n):
+    f = [0, 1]
+    if n >= 2:
+        for i in range(2, n+1):
+            f.append(f[i-1] + f[i-2])
+    return f[n]
+
+
+def main():
+    n = int(input())
+    # n = int(sys.stdin.readline())
+    print(fib(n))
+
+
+if __name__ == "__main__":
+    main()
